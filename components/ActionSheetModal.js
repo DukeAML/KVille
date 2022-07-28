cce73ad272c5b531a5483ef0d7b9988a690b85e8
kevin <kevinwenfu@gmail.com>
2022-07-27 20:39:33 -0400

    Set keyboardDismissMode

--- components/ActionSheetModal.js
+++ components/ActionSheetModal.js
@@ -3,51 +3,58 @@ import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView } from 'react-na
 import Modal from 'react-native-modal';
 import { useTheme } from '../context/ThemeProvider';
 
-
 /**
  * Parent Function for ActionSheet Modal component
- * 
+ *
  * @param {function} toggleModal -- if cancelButton is true, set parameter to toggleModal function
  * @param {number} height -- vertical size of modal
- * @param {color} backgroundColor -- background color of modal 
- * @param {boolean} swipeDown -- if you want the modal to be swipeable 
+ * @param {color} backgroundColor -- background color of modal
+ * @param {boolean} swipeDown -- if you want the modal to be swipeable
  *      * would recommend false if modal has scrollable components such as a picker
  *      * if true, must add onSwipeComplete prop to the modal
  * @param {string} userStyle -- options: ('dark' | 'light') background color of modal and text color
  * @param {boolean} cancelButton  -- if a cancel button should be added to the modal
- * @param {Component} children -- components inside modal component; 
- * 
+ * @param {Component} children -- components inside modal component;
+ *
  *
  * @param {props} props --
  *   **NOTE: must include normal modal props in the props of the Modal such as:
- *       isVisible={isModalVisible} 
+ *       isVisible={isModalVisible}
  *       onBackdropPress={() => setModalVisible(false)}
- *       onSwipeComplete = {toggleModal} 
+ *       onSwipeComplete = {toggleModal}
  *       ...
  * @returns Modal Component
  */
 
-const ActionSheetModal = ({ toggleModal , backgroundColor, swipeDown = true, height = 130, userStyle,
-                            cancelButton = false,  children, ...props  }) => {
+const ActionSheetModal = ({
+  toggleModal,
+  backgroundColor,
+  swipeDown = true,
+  height = 130,
+  userStyle,
+  cancelButton = false,
+  children,
+  ...props
+}) => {
   {
-    const {theme} = useTheme();
+    const { theme } = useTheme();
 
     //set up default styles for light and dark themes
     let background, opacity;
-    userStyle == 'light' ? (background = '#fff', opacity= '96%') : 
-            (background= '#565656', opacity = '100%');
+    userStyle == 'light' ? ((background = '#fff'), (opacity = '96%')) : ((background = '#565656'), (opacity = '100%'));
 
     //overwrite dark|light theme background colors if defined
-    if (backgroundColor !== undefined) background = backgroundColor; 
+    if (backgroundColor !== undefined) background = backgroundColor;
 
     //Height of entire modal based on if cancel button is added or not
-    const adjustedHeight = cancelButton ? height+110 : height+30; //accounts for vertical margin and cancel button
+    const adjustedHeight = cancelButton ? height + 110 : height + 30; //accounts for vertical margin and cancel button
 
     return (
       <Modal
         style={styles(theme).BottomModalView}
         swipeDirection={swipeDown ? ['down'] : null}
         backdropTransitionOutTiming={0}
+        keyboardDismissMode={'on-drag'}
         {...props}
       >
         <SafeAreaView style={{ width: '95%', alignSelf: 'center', height: adjustedHeight }}>
@@ -70,35 +77,37 @@ const ActionSheetModal = ({ toggleModal , backgroundColor, swipeDown = true, hei
   }
 };
 
-
-const styles = (theme) => StyleSheet.create({
-    BottomModalView:{
-        margin: 0,
-        justifyContent: 'flex-end',
+const styles = (theme) =>
+  StyleSheet.create({
+    BottomModalView: {
+      margin: 0,
+      justifyContent: 'flex-end',
     },
-    TopSectionView: { //Style for top view of action sheet
-        width: '100%',
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        borderRadius: 20,
-        margin: 15,
+    TopSectionView: {
+      //Style for top view of action sheet
+      width: '100%',
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      borderRadius: 20,
+      margin: 15,
     },
-    CancelBtn: { //style for Cancel Button
-        alignSelf: 'center',
-        width: '100%',
-        borderRadius: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 65,
-        marginBottom: 15,
+    CancelBtn: {
+      //style for Cancel Button
+      alignSelf: 'center',
+      width: '100%',
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 65,
+      marginBottom: 15,
     },
-    CancelBtnText: { //style for text of Cancel Button
-        fontWeight: '700',
-        textAlign: 'center',
-        fontSize: 24,
+    CancelBtnText: {
+      //style for text of Cancel Button
+      fontWeight: '700',
+      textAlign: 'center',
+      fontSize: 24,
     },
-});
-
+  });
 
 export { ActionSheetModal };
--- components/ConfirmationModal.js
+++ components/ConfirmationModal.js
@@ -21,73 +21,80 @@ import { useTheme } from '../context/ThemeProvider';
 
  * @returns Modal Component
  */
-const ConfirmationModal = ({ toggleModal , buttonAction, body, buttonText, userStyle, ...props  }) => {
+const ConfirmationModal = ({ toggleModal, buttonAction, body, buttonText, userStyle, ...props }) => {
   {
-    const {theme} = useTheme();
+    const { theme } = useTheme();
 
     //set up default styles for light and dark themes
     let backgroundColor, opacity, bodyText;
-    userStyle == 'light' ? (backgroundColor = '#fff', opacity= '92%', bodyText = 'black') : 
-            (backgroundColor= '#565656', opacity = '100%', bodyText = '#fff')
+    userStyle == 'light'
+      ? ((backgroundColor = '#fff'), (opacity = '92%'), (bodyText = 'black'))
+      : ((backgroundColor = '#565656'), (opacity = '100%'), (bodyText = '#fff'));
 
     return (
-        <Modal
-            style={styles(theme).BottomModalView}
-            swipeDirection={['down']}
-            backdropTransitionOutTiming={0}
-            {...props}
-        >
-            <View>
-                <View style={[styles(theme).confirmationPop, {backgroundColor: backgroundColor, opacity: opacity}]}>
-                    <View style = {{height: '60%', width: '100%', justifyContent:'center', borderBottomWidth: 1,
-                        borderBottomColor: '#cfcfcf', padding: 10}}>
-                        <Text style={[styles(theme).confirmationText, {color: bodyText}]}>
-                            {body}
-                        </Text>
-                    </View>
-
-                    <TouchableOpacity
-                        onPress={() => {
-                            buttonAction();
-                            toggleModal();
-                        }}
-                        style = {{height: '40%', width: '100%'}}
-                    >
-                        <View style = {{height: '100%', width: '100%', justifyContent:'center'}}>
-                            <Text style={[styles(theme).confirmationHeader, { color: theme.error}]}>
-                                {buttonText}
-                            </Text>
-                        </View>
-                        
-                    </TouchableOpacity>
-                </View>
-
-                <TouchableOpacity
-                    onPress= {()=> toggleModal()}
-                    style={[styles(theme).confirmationBottomBtn, {backgroundColor: backgroundColor, opacity: opacity}]}
-                >
-                    <Text style= {[styles(theme).confirmationHeader, { color: '#1988f8' }]}>Cancel</Text>
-                </TouchableOpacity>
+      <Modal
+        style={styles(theme).BottomModalView}
+        swipeDirection={['down']}
+        backdropTransitionOutTiming={0}
+        keyboardDismissMode={'on-drag'}
+        {...props}
+      >
+        <View>
+          <View style={[styles(theme).confirmationPop, { backgroundColor: backgroundColor, opacity: opacity }]}>
+            <View
+              style={{
+                height: '60%',
+                width: '100%',
+                justifyContent: 'center',
+                borderBottomWidth: 1,
+                borderBottomColor: '#cfcfcf',
+                padding: 10,
+              }}
+            >
+              <Text style={[styles(theme).confirmationText, { color: bodyText }]}>{body}</Text>
             </View>
+
+            <TouchableOpacity
+              onPress={() => {
+                buttonAction();
+                toggleModal();
+              }}
+              style={{ height: '40%', width: '100%' }}
+            >
+              <View style={{ height: '100%', width: '100%', justifyContent: 'center' }}>
+                <Text style={[styles(theme).confirmationHeader, { color: theme.error }]}>{buttonText}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => toggleModal()}
+            style={[styles(theme).confirmationBottomBtn, { backgroundColor: backgroundColor, opacity: opacity }]}
+          >
+            <Text style={[styles(theme).confirmationHeader, { color: '#1988f8' }]}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
       </Modal>
     );
   }
 };
 
-
-const styles = (theme) => StyleSheet.create({
-    BottomModalView:{ //Modal Style for placing at bottom
-        margin: 0,
-        justifyContent: 'flex-end',
+const styles = (theme) =>
+  StyleSheet.create({
+    BottomModalView: {
+      //Modal Style for placing at bottom
+      margin: 0,
+      justifyContent: 'flex-end',
     },
-    confirmationPop: { //Style for top part container
-        width: '95%',
-        height: 130,
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        borderRadius: 20,
-        margin: 15,
+    confirmationPop: {
+      //Style for top part container
+      width: '95%',
+      height: 130,
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      borderRadius: 20,
+      margin: 15,
 
       // shadowColor: '#171717',
       // shadowOffset: { width: 0, height: -5 },
@@ -95,31 +102,34 @@ const styles = (theme) => StyleSheet.create({
       // shadowRadius: 20,
       // elevation: 5,
     },
-    confirmationHeader: { //style for text of buttons
-        fontWeight: '600',
-        textAlign: 'center',
-        fontSize: 23,
+    confirmationHeader: {
+      //style for text of buttons
+      fontWeight: '600',
+      textAlign: 'center',
+      fontSize: 23,
     },
-    confirmationText: { //style for prompt text
-        textAlign: 'center',
-        justifyContent: 'center',
-        padding: 5,
+    confirmationText: {
+      //style for prompt text
+      textAlign: 'center',
+      justifyContent: 'center',
+      padding: 5,
     },
-    confirmationBottomBtn: { //style for cancel button
-        color: theme.text1,
-        alignSelf: 'center',
-        width: '95%',
-        borderRadius: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 65,
-        marginBottom: 15,
+    confirmationBottomBtn: {
+      //style for cancel button
+      color: theme.text1,
+      alignSelf: 'center',
+      width: '95%',
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 65,
+      marginBottom: 15,
 
-        shadowColor: '#171717',
-        shadowOffset: { width: 0, height: -5 },
-        shadowOpacity: 0.5,
-        shadowRadius: 20,
-        elevation: 5,
+      shadowColor: '#171717',
+      shadowOffset: { width: 0, height: -5 },
+      shadowOpacity: 0.5,
+      shadowRadius: 20,
+      elevation: 5,
     },
   });
 
