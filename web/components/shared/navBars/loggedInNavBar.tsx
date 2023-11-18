93326683e363c1e312e6da078bbdcbf47ef57a7d
Keith Cressman <keith.cressman@duke.edu>
2023-11-17 22:28:00 -0500

    added sign out to navbar

--- web/components/shared/navBars/loggedInNavBar.tsx
+++ web/components/shared/navBars/loggedInNavBar.tsx
@@ -3,6 +3,7 @@ import { KvilleNavBarGivenOptionDescriptions, OptionDescription } from "./navBar
 export const KvilleLoggedInNavBar : React.FC = () => {
     let buttonDescriptions : OptionDescription[] = [
         {text : "My Groups", route : "/groups"},
+        {text : "Sign Out", route : "/signOut"}
     ]
     return <KvilleNavBarGivenOptionDescriptions optionDescriptions={buttonDescriptions}/>;
 }
