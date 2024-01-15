69d04b0d405d3b79ba11850e0855019353cd506a
Keith Cressman <keith.cressman@duke.edu>
2024-01-15 15:05:07 -0500

    added footer with link to question form

--- web/components/shared/pageContainers/basePageContainer.tsx
+++ web/components/shared/pageContainers/basePageContainer.tsx
@@ -1,10 +1,6 @@
 import React, { ReactNode, useContext, useEffect} from 'react';
 import { UserContext } from '@/lib/context/userContext';
-import {
-  Container,
-  Typography,
-
-} from '@material-ui/core';
+import { Container, Typography } from '@mui/material';
 
 
 import { KvilleLoggedInNavBar } from '../navBars/loggedInNavBar';
@@ -12,9 +8,7 @@ import { KvilleLoggedOutNavBar } from '../navBars/loggedOutNavBar';
 import { auth } from '@/lib/db/firebase_config';
 import { onAuthStateChanged } from 'firebase/auth';
 import { INVALID_USER_ID } from '@/lib/db/auth/login';
-
-
-
+import Footer from '../utils/footer';
 
 interface BasePageContainerProps {
     children: ReactNode;
@@ -42,9 +36,25 @@ export const BasePageContainer: React.FC<BasePageContainerProps> = (props:BasePa
 	});
 	
 	return (
-		<Container component="main" maxWidth={false} style={{marginBottom : 8}} >
-			{props.children? props.children : <></>}
-		</Container>
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				minHeight: '100vh',
+			}}
+		>
+			<Container
+				component="main"
+				maxWidth={false}
+				style={{
+					flex: 1,
+					marginBottom: 8,
+				}}
+			>
+				{props.children ? props.children : <></>}
+			</Container>
+			<Footer />
+		</div>
 	);
 };
 
